9edaa2aa062ac32766ae220fada79d21e891f873
monkeyDluzi <Luziandre588@gmail.com>
2026-05-21 11:37:50 -0400

    Update project.js

--- project.js
+++ project.js
@@ -1,1 +1,12 @@
 
+const playButton = document.getElementById("goAi")
+const homepageSection = document.getElementById("homepage")
+const vsAISection = document.getElementById("vsAI")
+
+
+function showAiSection() {
+  homepageSection.style.display = 'none';
+  vsAISection.style.display = 'flex';
+  
+}
+
