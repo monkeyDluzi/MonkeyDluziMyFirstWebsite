d1f58e644da4fb78ec555a8657140cb69702b5b9
monkeyDluzi <Luziandre588@gmail.com>
2026-05-21 16:26:01 -0400

    Update project.js

--- project.js
+++ project.js
@@ -3,10 +3,10 @@ const playButton = document.getElementById("goAi")
 const homepageSection = document.getElementById("homepage")
 const vsAISection = document.getElementById("vsAI")
 
-
 function showAiSection() {
   homepageSection.style.display = 'none';
   vsAISection.style.display = 'flex';
-  
 }
 
+// FIX: Listen for the click eve
+playButton.addEventListener("click", showAiSection);
